dccfdae4dd44db729bf51a03ea13bf79211927ab
Mike Lester <themikelester@gmail.com>
2026-01-25 00:36:50 -0700

    Ship spawns pirates

--- src/ZeldaWindWaker/d_a.ts
+++ src/ZeldaWindWaker/d_a.ts
@@ -7226,6 +7226,30 @@ class d_a_obj_pirateship extends fopAc_ac_c {
         const modelData = globals.resCtrl.getObjectRes(ResType.Model, d_a_obj_pirateship.arcName, 0xE);
         this.model = new J3DModelInstance(modelData);
 
+        this.partsCreate();
+
+        const pirateData: [string, number, vec3, number][] = [
+            ["P2a", 0xFFFFFF00, vec3.fromValues(135.5, 2200.0, 198.0), 0x2900],
+            ["P2c", 0xFFFFFF02, vec3.fromValues(275.0, 400.0, 216.0), 0x4D00],
+            ["P1b", 0xF0100000, vec3.fromValues(-150.0, 700.0, 950.0), 0x4000],
+            ["P1a", 0xF0000000, vec3.fromValues(150.0, 700.0, 950.0), 0xC000],
+            ["Zl1", 0xFFFFFF04, vec3.fromValues(-200.0, 680.0, -875.0), 0],
+            ["P1b", 0xF0100002, vec3.fromValues(163.0, 700.0, 822.0), 0xC000],
+            ["P1a", 0xF0000002, vec3.fromValues(0.0, 750.0, -860.0), 0],
+            ["Zl1", 0xFFFFFF03, vec3.fromValues(125.0, 2200.0, 100.0), 0],
+            ["P2a", 0xFFFFFF00, vec3.fromValues(-200.0, 680.0, -875.0), 0x0000],
+            ["P1b", 0xF0100003, vec3.fromValues(215.0, 700.0, 765.0), 0x3C00],
+            ["P1a", 0xF0000003, vec3.fromValues(275.0, 740.0, -1145.0), 0x1800],
+        ];
+
+        const shipCfgIdx = (this.parameters >> 0x18) & 0xFF;
+        switch (shipCfgIdx) {
+            case 0: ([0, 1, 2, 3]).forEach(p => this.pirateCreate(globals, ...pirateData[p])); break;
+            case 1: ([0, 1, 4, 5, 6]).forEach(p => this.pirateCreate(globals, ...pirateData[p])); break;
+            case 2: ([1, 7, 8, 9, 10]).forEach(p => this.pirateCreate(globals, ...pirateData[p])); break;
+            case 4: ([0, 1, 5]).forEach(p => this.pirateCreate(globals, ...pirateData[p])); break;
+        };
+
         this.set_mtx();
 
         return cPhs__Status.Next;
@@ -7236,6 +7260,23 @@ class d_a_obj_pirateship extends fopAc_ac_c {
         setLightTevColorType(globals, this.model, this.tevStr, globals.camera);
         mDoExt_modelUpdateDL(globals, this.model, renderInstManager, globals.dlst.bg);
     }
+
+    private partsCreate(): void {
+        // TODO
+    }
+
+    private pirateCreate(globals: dGlobals, name: string, parameters: number, posOffset: vec3, rotYOffset: number) {
+        const objName = assertExists(globals.dStage_searchName(name));
+        vec3.rotateY(posOffset, posOffset, Vec3Zero, cM_s2rad(this.rot[1]));
+        const pos = vec3.add(posOffset, this.pos, posOffset);
+        const rot = vec3.fromValues(this.rot[0], this.rot[1] + rotYOffset, this.rot[2]);
+
+        // Make sure the actors are created on the same noclip roomLayer, so they only appear on the same layer as the ship
+        const prm: fopAcM_prm_class = { parameters, pos, roomNo: this.tevStr.roomNo, rot, scale: Vec3One, subtype: objName.subtype,  
+            parentPcId: this.processId, enemyNo: -1, gbaName: objName.gbaName, layer: this.roomLayer };
+        fpcSCtRq_Request(globals.frameworkGlobals, null, objName.pcName, prm);
+    };
+        
 
     private set_mtx(): void {
         vec3.copy(this.model.baseScale, this.scale);
